0a2bcffeb7a30b592c0c46e10f86da2e2c84be04
UmaAshaMS <umaasha69@gmail.com>
2024-04-04 22:33:41 +0530

    Form updation

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu() {
     menu.classList.toggle("open");
     icon.classList.toggle("open");
   }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzn58SLHjTo6AVa9kpqdAwOY75urtSEgYTUUuDzO0JORkqx_KZfoUmDfC3Gw-3RDBdUKw/exec'
+
+const form = document.forms['submit-to-google-sheet']
+
+
+form.addEventListener('submit', e => {
+
+  e.preventDefault()
+
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+
+    .then(response => console.log('Success!', response))
+
+    .catch(error => console.error('Error!', error.message))
+
+})
